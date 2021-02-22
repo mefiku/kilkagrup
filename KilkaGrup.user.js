5988de517c1967f8c26874302a5ad85950df7266
mefiku <35295541+mefiku@users.noreply.github.com>
2021-02-22 15:01:48 +0100

    Add files via upload

---
+++ KilkaGrup.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         KilkaGrup
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  dodaje możliwość wyboru kilku grup wykładowych oraz laboratoryjnych (po dwie)
+// @author       mefiuk
+// @match        *://www.pwsz.nysa.pl/plany/planWydruk.php*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+var w = document.getElementsByTagName('label')[0];
+var l = document.getElementsByTagName('label')[1];
+var cln = w.cloneNode(true);
+w.appendChild(cln);
+cln = l.cloneNode(true);
+l.appendChild(cln);
+})();
